204e643d4206c4714c6bb61fd43fd52eb6d9b5dd
colinauyeung <daisuke.moto403@gmail.com>
2023-08-31 13:19:41 -0600

    File Update

--- app.js
+++ app.js
@@ -1,46 +1,117 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+// const { Client, GatewayIntentBits } = require('discord.js');
 require("dotenv").config();
 
-const WebSocket = require('ws');
+const fs = require('fs/promises');
 
-const ws = new WebSocket("ws://localhost:8080");
+const WebSocket = require('ws')
+const WebSocketServer = WebSocket.WebSocketServer;
 
-ws.addEventListener("open", () => {
+const files = ["parsedData.json","parsedData.json","parsedData.json","parsedData.json","parsedData.json","parsedData.json"]
+
+const wss = new WebSocketServer({ port: 8080 });
+
+function foo(ws, index,jsdata, lcchannel){
+    
+    var rs = {
+        content: jsdata["comments"][index]["message"],
+        username: jsdata["comments"][index]["name"],
+        channel: lcchannel
+    }
+    ws.send(JSON.stringify(rs));
+    index++;
+    if(index<jsdata["comments"].length){
+        setTimeout(e=>{
+            foo(ws, index, jsdata, lcchannel)
+        }, 500+(Math.random()*1000))
+    }
+
+}
+
+
+wss.on('connection', function connection(ws) {
     console.log("We are connected");
-});
+    ws.on('message', function message(data) {
+      console.log('received: %s', data);
+    });
 
-ws.addEventListener('message', function (event) {
-    console.log(event.data);
-});
+    fs.readFile(files[0])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "a")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ]
-});
+    fs.readFile(files[1])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "b")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+    fs.readFile(files[2])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "c")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-client.on('messageCreate', message => {
-    console.log(`New message in ${message.channel.name} from ${message.author.tag}: ${message.content}`);
 
-    var username = message.author.tag.split('#')[0]
-    var content = {
-        "username": username,
-        "channel": message.channel.name,
-        "content": message.content
-    }
+    fs.readFile(files[3])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "d")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-    ws.send(JSON.stringify(content))
+    fs.readFile(files[4])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "e")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-    if (message.content === 'ping') { //read messages
-        message.channel.send('pong');
-    }
-});
+    fs.readFile(files[5])
+    .then(data => {
+        var jsdata = JSON.parse(data)
+        var index = 0;
+        setTimeout(e=>{
+            foo(ws, index, jsdata, "f")
+        }, 500+Math.random()*500)
+    })
+    .catch((error) => {
+        console.error(error);
+        // Do something if error 
+    });
 
-client.login(process.env.DISCORD_TOKEN);
+  });
 
